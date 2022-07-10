910bafb6bb359157e1bf1873837f15af03a37518
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2022-07-10 12:31:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+### Generate a website in Python
+
