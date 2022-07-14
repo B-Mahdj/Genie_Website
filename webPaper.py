fef83327da2307ad4f6fcadd86860e53341f3592
B-Mahdj <bilal.mahdjoubi95@gmail.com>
2022-07-14 12:23:56 +0200

    Refactor the part 1 of the code

--- webPaper.py
+++ webPaper.py
@@ -3,7 +3,7 @@
 """
 Created on Sat Jul  9 16:07:46 2022
 
-@author: sohanjs
+@author: sohanjs, Mahdjoubi Bilal
 """
 
 # Import the libraries
@@ -13,19 +13,21 @@ import openai
 import glob
 import re
 import json
+import os
 
-api_key = "wuQ7GVHRhe1qCJljF8TPOyxnmbtSDdIL"
-api_endpoint = "https://api.core.ac.uk/v3"
+API_KEY_CORE_API = "wuQ7GVHRhe1qCJljF8TPOyxnmbtSDdIL"
+API_ENDPOINT_CORE_API = "https://api.core.ac.uk/v3"
+OUTPUT_PDF = '.\pdfs'
 
 # Set initial Parameters
 # Enter your OpenAI API keys to run GPT-3 model
 # Remember to authorize the key before using it.
-openai.api_key = "YOUR_APP_KEY"
+openai.api_key = "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"
 
-## Charater_limit is set in order to avoid the maxing token request
+# Character_limit is set in order to avoid the maxing token request
 CHARACTER_LIMIT = 3000
 
-## how many number of pdf downloads are needed ?
+# how many number of pdf downloads are needed ?
 NUMBER_OF_PDF_DOWNLOADS = 5
 
 
@@ -36,31 +38,32 @@ def pretty_json(obj):
 # ---------------------- PART 1 ----------------------
 # This function receive a topic and search for the papers in the topic
 # It will download the papers and save them in the folder named "papers"
-# The files will be download for the CORE API that is an API that is used to search for research papers
+# The files will be downloaded for the CORE API that is an API that is used to get research papers
 
 def get_papers(topic):
-    results, elapsed = query_core_api("/search/outputs", topic)
+    results, elapsed = query_core_api("/search/works", topic)
     print(f"The search took {elapsed} seconds")
     print(f"The results are: ")
     pretty_json(results)
     for result in results["results"]:
-        print(f"Result is : {result}")
-        download_pdf(result.sourceFulltextUrls[0])
+        download_pdf(result["downloadUrl"])
 
 
 def download_pdf(url):
     response = requests.get(url)
     if response.status_code == 200:
-        return response.content
+        file_path = os.path.join(OUTPUT_PDF, os.path.basename(url))
+        with open(file_path, "wb") as file:
+            file.write(response.content)
     else:
         print(f"Error code {response.status_code}, {response.content}")
         handle_error(response.status_code)
 
 
 def query_core_api(url_fragment, query, limit=NUMBER_OF_PDF_DOWNLOADS):
-    headers = {"Authorization": "Bearer " + api_key}
+    headers = {"Authorization": "Bearer " + API_KEY_CORE_API}
     query = {"q": query, "limit": limit}
-    response = requests.post(f"{api_endpoint}{url_fragment}", data=json.dumps(query), headers=headers)
+    response = requests.post(f"{API_ENDPOINT_CORE_API}{url_fragment}", data=json.dumps(query), headers=headers)
     if response.status_code == 200:
         return response.json(), response.elapsed.total_seconds()
     else:
@@ -71,14 +74,7 @@ def query_core_api(url_fragment, query, limit=NUMBER_OF_PDF_DOWNLOADS):
 # ---------------------- PART 2 ----------------------
 #
 
-# Reads all the pdf in the current directory
-files = glob.glob("*.pdf")
 
-
-# displays the paper Content
-def displayPaperContent(paperContent, page_start=0, page_end=5):
-    for page in paperContent[page_start:page_end]:
-        print(page.extract_text())
 
 
 # Shows the Paper Summary from GPT-3
@@ -87,7 +83,7 @@ def showPaperSummary(paperContent):
     text = ""
 
     numberPages = paperContent.pages
-    ##Loop through all the pages of the paper and concatenate the text
+    # Loop through all the pages of the paper and concatenate the text
     for page in numberPages:
         text += page.extract_text()
 
@@ -102,7 +98,7 @@ def showPaperSummary(paperContent):
         textEnd = re.search("(?=CONCLUSION\n|CONCLUSIONS\n)[\s\S]*", text).group()
     except AttributeError:
         textEnd = re.search("(?=CONCLUSION\n|CONCLUSIONS\n)[\s\S]*", text)
-    # print("The text after Conclusion is : ", textEnd)
+
     if textBegin is not None and textEnd is not None:
         text = textBegin + textEnd
     if text is not None:
@@ -121,30 +117,28 @@ def showPaperSummary(paperContent):
     print(response["choices"][0]["text"])
 
 
-##Make sure the numbers of characters in text is under or equals the limited charater
+# Make sure the numbers of characters in text is under or equals the limited character
 def cut(text):
-    ###If it is, then we return the text
-    ###If it is not, then we cut the text and return the text
+    # If it is, then we return the text
+    # If it is not, then we cut the text and return the text
     if len(text) <= CHARACTER_LIMIT:
-        # print("Length text : , good", len(text))
         return text
     else:
-        # print("Length text : , bad", len(text))
         return text[:CHARACTER_LIMIT]
-
-
-# reads all the pdf files in the folder
-for f in files:
-    print()
-    print(f)
-    paperContent = PdfFileReader(f)
-    showPaperSummary(paperContent)
 
 
 def main():
     get_papers("Machine Learning")
-    return None
 
+    # Reads all the pdf in the pdfs directory
+    files = glob.glob(OUTPUT_PDF + "/*.pdf")
+
+    # reads all the pdf files in the folder
+    for f in files:
+        print()
+        print(f)
+        paperContent = PdfFileReader(f)
+        showPaperSummary(paperContent)
 
 def handle_error(status_code):
     pass
